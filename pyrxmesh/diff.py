1a5916465a2f1bd1cb1520993c3cf4dda1713fb9
ahmed <a.has.mahmoud@gmail.com>
2026-05-27 22:29:02 -0400

    Create diff.py

---
+++ pyrxmesh/diff.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from . import CandidatePairs, HessianSparseMatrix, JacobianSparseMatrix
+
+__all__ = [
+    "CandidatePairs",
+    "HessianSparseMatrix",
+    "JacobianSparseMatrix",
+]
